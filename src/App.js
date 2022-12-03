2df0b2749ac63a38696e61912542053c4918247f
jithstephen13 <jithstephen96@gma.com>
2022-12-03 18:09:38 +0530

    add counter

--- src/App.js
+++ src/App.js
@@ -7,10 +7,38 @@ function App() {
   const[count,setCount]=useState(0)
 
   return (
-    <div >
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '300%',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: '-15%',
+    }} >
       <h1>{count}</h1>
-      <button onClick={()=>setCount(count+1)}>INC</button>
-      <button onClick={()=>setCount(count-1)}>DEC</button>
+      <button onClick={()=>setCount(count+1)}
+        style={{
+          fontSize: '60%',
+          position: 'relative',
+          top: '20vh',
+          marginLeft: '5px',
+          backgroundColor: 'red',
+          borderRadius: '8%',
+          color: 'white',}}
+      >INC</button>
+      <button onClick={()=>setCount(count-1)}
+      style={{
+        fontSize: '60%',
+        position: 'relative',
+        top: '20vh',
+        marginRight: '5px',
+        backgroundColor: 'green',
+        borderRadius: '8%',
+        color: 'white',
+      }}>DEC</button>
       
     </div>
   );
